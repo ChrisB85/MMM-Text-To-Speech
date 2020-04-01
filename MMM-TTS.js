909132564c8f8b69eb3dcf8bb20bf63d715c3ad8
Krzysztof Błachowicz <krzysztof.blachowicz@gmail.com>
2020-04-01 11:31:52 +0200

    first try

--- MMM-TTS.js
+++ MMM-TTS.js
@@ -8,150 +8,151 @@
  */
 
 Module.register("MMM-TTS", {
-	defaults: {
-		updateInterval: 60000,
-		retryDelay: 5000
-	},
+    defaults: {
+        updateInterval: 60000,
+        retryDelay: 5000
+    },
 
-	requiresVersion: "2.1.0", // Required version of MagicMirror
+    requiresVersion: "2.1.0", // Required version of MagicMirror
 
-	start: function() {
-		var self = this;
-		var dataRequest = null;
-		var dataNotification = null;
+    start: function () {
+        var self = this;
+        var dataRequest = null;
+        var dataNotification = null;
 
-		//Flag for check if module is loaded
-		this.loaded = false;
+        //Flag for check if module is loaded
+        this.loaded = false;
 
-		// Schedule update timer.
-		this.getData();
-		setInterval(function() {
-			self.updateDom();
-		}, this.config.updateInterval);
-	},
+        // Schedule update timer.
+        this.getData();
+        setInterval(function () {
+            self.updateDom();
+        }, this.config.updateInterval);
+    },
 
-	/*
-	 * getData
-	 * function example return data and show it in the module wrapper
-	 * get a URL request
-	 *
-	 */
-	getData: function() {
-		var self = this;
+    /*
+     * getData
+     * function example return data and show it in the module wrapper
+     * get a URL request
+     *
+     */
+    getData: function () {
+        var self = this;
 
-		var urlApi = "https://jsonplaceholder.typicode.com/posts/1";
-		var retry = true;
+        var urlApi = "https://jsonplaceholder.typicode.com/posts/1";
+        var retry = true;
 
-		var dataRequest = new XMLHttpRequest();
-		dataRequest.open("GET", urlApi, true);
-		dataRequest.onreadystatechange = function() {
-			console.log(this.readyState);
-			if (this.readyState === 4) {
-				console.log(this.status);
-				if (this.status === 200) {
-					self.processData(JSON.parse(this.response));
-				} else if (this.status === 401) {
-					self.updateDom(self.config.animationSpeed);
-					Log.error(self.name, this.status);
-					retry = false;
-				} else {
-					Log.error(self.name, "Could not load data.");
-				}
-				if (retry) {
-					self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
-				}
-			}
-		};
-		dataRequest.send();
-	},
+        var dataRequest = new XMLHttpRequest();
+        dataRequest.open("GET", urlApi, true);
+        dataRequest.onreadystatechange = function () {
+            console.log(this.readyState);
+            if (this.readyState === 4) {
+                console.log(this.status);
+                if (this.status === 200) {
+                    self.processData(JSON.parse(this.response));
+                } else if (this.status === 401) {
+                    self.updateDom(self.config.animationSpeed);
+                    Log.error(self.name, this.status);
+                    retry = false;
+                } else {
+                    Log.error(self.name, "Could not load data.");
+                }
+                if (retry) {
+                    self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
+                }
+            }
+        };
+        dataRequest.send();
+    },
+
+    /* scheduleUpdate()
+     * Schedule next update.
+     *
+     * argument delay number - Milliseconds before next update.
+     *  If empty, this.config.updateInterval is used.
+     */
+    scheduleUpdate: function (delay) {
+        var nextLoad = this.config.updateInterval;
+        if (typeof delay !== "undefined" && delay >= 0) {
+            nextLoad = delay;
+        }
+        nextLoad = nextLoad;
+        var self = this;
+        setTimeout(function () {
+            self.getData();
+        }, nextLoad);
+    },
+
+    getDom: function () {
+        var self = this;
+
+        // create element wrapper for show into the module
+        var wrapper = document.createElement("div");
+        // If this.dataRequest is not empty
+        if (this.dataRequest) {
+            var wrapperDataRequest = document.createElement("div");
+            // check format https://jsonplaceholder.typicode.com/posts/1
+            wrapperDataRequest.innerHTML = this.dataRequest.title;
+
+            var labelDataRequest = document.createElement("label");
+            // Use translate function
+            //             this id defined in translations files
+            labelDataRequest.innerHTML = this.translate("TITLE");
 
 
-	/* scheduleUpdate()
-	 * Schedule next update.
-	 *
-	 * argument delay number - Milliseconds before next update.
-	 *  If empty, this.config.updateInterval is used.
-	 */
-	scheduleUpdate: function(delay) {
-		var nextLoad = this.config.updateInterval;
-		if (typeof delay !== "undefined" && delay >= 0) {
-			nextLoad = delay;
-		}
-		nextLoad = nextLoad ;
-		var self = this;
-		setTimeout(function() {
-			self.getData();
-		}, nextLoad);
-	},
+            wrapper.appendChild(labelDataRequest);
+            wrapper.appendChild(wrapperDataRequest);
+        }
 
-	getDom: function() {
-		var self = this;
+        // Data from helper
+        if (this.dataNotification) {
+            var wrapperDataNotification = document.createElement("div");
+            // translations  + datanotification
+            wrapperDataNotification.innerHTML = this.translate("UPDATE") + ": " + this.dataNotification.date;
 
-		// create element wrapper for show into the module
-		var wrapper = document.createElement("div");
-		// If this.dataRequest is not empty
-		if (this.dataRequest) {
-			var wrapperDataRequest = document.createElement("div");
-			// check format https://jsonplaceholder.typicode.com/posts/1
-			wrapperDataRequest.innerHTML = this.dataRequest.title;
+            wrapper.appendChild(wrapperDataNotification);
+        }
+        return wrapper;
+    },
 
-			var labelDataRequest = document.createElement("label");
-			// Use translate function
-			//             this id defined in translations files
-			labelDataRequest.innerHTML = this.translate("TITLE");
+    getScripts: function () {
+        return [];
+    },
 
+    getStyles: function () {
+        return [
+            "MMM-TTS.css",
+        ];
+    },
 
-			wrapper.appendChild(labelDataRequest);
-			wrapper.appendChild(wrapperDataRequest);
-		}
+    // Load translations files
+    getTranslations: function () {
+        //FIXME: This can be load a one file javascript definition
+        return {
+            en: "translations/en.json",
+            es: "translations/es.json"
+        };
+    },
 
-		// Data from helper
-		if (this.dataNotification) {
-			var wrapperDataNotification = document.createElement("div");
-			// translations  + datanotification
-			wrapperDataNotification.innerHTML =  this.translate("UPDATE") + ": " + this.dataNotification.date;
+    processData: function (data) {
+        var self = this;
+        this.dataRequest = data;
+        if (this.loaded === false) {
+            self.updateDom(self.config.animationSpeed);
+        }
+        this.loaded = true;
 
-			wrapper.appendChild(wrapperDataNotification);
-		}
-		return wrapper;
-	},
+        // the data if load
+        // send notification to helper
+        this.sendSocketNotification("MMM-TTS-NOTIFICATION_TEST", data);
+    },
 
-	getScripts: function() {
-		return [];
-	},
-
-	getStyles: function () {
-		return [
-			"MMM-TTS.css",
-		];
-	},
-
-	// Load translations files
-	getTranslations: function() {
-		//FIXME: This can be load a one file javascript definition
-		return {
-			en: "translations/en.json",
-			es: "translations/es.json"
-		};
-	},
-
-	processData: function(data) {
-		var self = this;
-		this.dataRequest = data;
-		if (this.loaded === false) { self.updateDom(self.config.animationSpeed) ; }
-		this.loaded = true;
-
-		// the data if load
-		// send notification to helper
-		this.sendSocketNotification("MMM-TTS-NOTIFICATION_TEST", data);
-	},
-
-	// socketNotificationReceived from helper
-	socketNotificationReceived: function (notification, payload) {
-		if(notification === "MMM-TTS-NOTIFICATION_TEST") {
-			// set dataNotification
-			this.dataNotification = payload;
-			this.updateDom();
-		}
-	},
+    // socketNotificationReceived from helper
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === "MMM-TTS-NOTIFICATION_TEST") {
+            // set dataNotification
+            this.dataNotification = payload;
+            this.updateDom();
+        }
+    },
 });
--- node_helper.js
+++ node_helper.js
@@ -5,43 +5,128 @@
  * MIT Licensed.
  */
 
+require('es6-promise').polyfill();
+
 var NodeHelper = require("node_helper");
+var fs = require('fs');
+var path = require('path');
+var http = require('http');
+var https = require('https');
+var urlParse = require('url').parse;
+var googleTTS = require('google-tts-api');
+var md5 = require('md5');
 
 module.exports = NodeHelper.create({
 
-	// Override socketNotificationReceived method.
+    downloadFile: function (url, dest) {
+        return new Promise(function (resolve, reject) {
+            var info = urlParse(url);
+            var httpClient = info.protocol === 'https:' ? https : http;
+            var options = {
+                host: info.host,
+                path: info.path,
+                headers: {
+                    'user-agent': 'WHAT_EVER'
+                }
+            };
+            httpClient.get(options, function (res) {
+                // check status code
+                if (res.statusCode !== 200) {
+                    reject(new Error('request to ' + url + ' failed, status code = ' + res.statusCode + ' (' + res.statusMessage + ')'));
+                    return;
+                }
 
-	/* socketNotificationReceived(notification, payload)
-	 * This method is called when a socket notification arrives.
-	 *
-	 * argument notification string - The identifier of the noitication.
-	 * argument payload mixed - The payload of the notification.
-	 */
-	socketNotificationReceived: function(notification, payload) {
-		if (notification === "MMM-TTS-NOTIFICATION_TEST") {
-			console.log("Working notification system. Notification:", notification, "payload: ", payload);
-			// Send notification
-			this.sendNotificationTest(this.anotherFunction()); //Is possible send objects :)
-		}
-	},
+                var file = fs.createWriteStream(dest);
+                file.on('finish', function () {
+                    // close() is async, call resolve after close completes.
+                    file.close(resolve);
+                });
+                file.on('error', function (err) {
+                    // Delete the file async. (But we don't check the result)
+                    fs.unlink(dest);
+                    reject(err);
+                });
+                res.pipe(file);
+            })
+                    .on('error', function (err) {
+                        reject(err);
+                    })
+                    .end();
+        });
+    },
 
-	// Example function send notification test
-	sendNotificationTest: function(payload) {
-		this.sendSocketNotification("MMM-TTS-NOTIFICATION_TEST", payload);
-	},
+    tts: function (text) {
+        googleTTS(text)
+                .then(function (url) {
+                    console.log(url); // https://translate.google.com/translate_tts?...
+                    var fileName = md5(text);
+                    var destDir = path.resolve(__dirname, '../MMM-Sounds/sounds/tts/');
+                    if (!fs.existsSync(destDir)) {
+                        fs.mkdirSync(destDir);
+                    }
+                    var destFile = path.resolve(__dirname, fileName + '.mp3'); // file destination
+                    var outFile = path.resolve(destDir, fileName + '.wav');
+                    console.log('Download to ' + destFile + ' ...');
 
-	// this you can create extra routes for your module
-	extraRoutes: function() {
-		var self = this;
-		this.expressApp.get("/MMM-TTS/extra_route", function(req, res) {
-			// call another function
-			values = self.anotherFunction();
-			res.send(values);
-		});
-	},
+                    const Lame = require("node-lame").Lame;
+                    const decoder = new Lame({
+                        output: outFile
+                    }).setFile(destFile);
 
-	// Test another function
-	anotherFunction: function() {
-		return {date: new Date()};
-	}
+                    decoder
+                            .decode()
+                            .then(() => {
+                                // Decoding finished
+                                fs.unlink(destFile);
+                            })
+                            .catch(error => {
+                                // Something went wrong
+                            });
+
+                    return downloadFile(url, destFile);
+                })
+                .then(function () {
+                    console.log('Download success');
+                })
+                .catch(function (err) {
+                    console.error(err.stack);
+                });
+
+    },
+
+    // Override socketNotificationReceived method.
+
+    /* socketNotificationReceived(notification, payload)
+     * This method is called when a socket notification arrives.
+     *
+     * argument notification string - The identifier of the noitication.
+     * argument payload mixed - The payload of the notification.
+     */
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === "MMM-TTS") {
+            console.log("Working notification system. Notification:", notification, "payload: ", payload);
+            // Send notification
+            this.tts(payload);
+        }
+    },
+
+    // Example function send notification test
+    sendNotificationTest: function (payload) {
+//        this.sendSocketNotification("MMM-TTS", payload);
+    },
+
+    // this you can create extra routes for your module
+    extraRoutes: function () {
+        var self = this;
+        this.expressApp.get("/MMM-TTS/extra_route", function (req, res) {
+            // call another function
+            values = self.anotherFunction();
+            res.send(values);
+        });
+    },
+
+    // Test another function
+    anotherFunction: function () {
+        return {date: new Date()};
+    }
 });
