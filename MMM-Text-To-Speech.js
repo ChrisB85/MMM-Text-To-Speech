991a20da556ba8f5c0431f0a4400a234948440b4
Krzysztof Błachowicz <krzysztof.blachowicz@gmail.com>
2020-04-12 14:34:25 +0200

    code format

--- MMM-Text-To-Speech.js
+++ MMM-Text-To-Speech.js
@@ -9,16 +9,16 @@
 
 Module.register("MMM-Text-To-Speech", {
     defaults: {
-        language: 'en',
+        language: "en",
         speed: 1,
-        soundModuleDir: 'MMM-Sounds'
+        soundModuleDir: "MMM-Sounds",
     },
 
     requiresVersion: "2.1.0", // Required version of MagicMirror
 
     start: function () {
-        Log.info('Starting module: ' + this.name);
-        this.sendSocketNotification('CONFIG', this.config);
+        Log.info("Starting module: " + this.name);
+        this.sendSocketNotification("CONFIG", this.config);
     },
 
     getScripts: function () {
@@ -26,21 +26,19 @@ Module.register("MMM-Text-To-Speech", {
     },
 
     getStyles: function () {
-        return [
-            "MMM-Text-To-Speech.css",
-        ];
+        return ["MMM-Text-To-Speech.css"];
     },
 
     // Load translations files
     getTranslations: function () {
         return {
             en: "translations/en.json",
-            es: "translations/es.json"
+            es: "translations/es.json",
         };
     },
 
     notificationReceived: function (notification, payload, sender) {
-        if (notification === 'MMM-Text-To-Speech') {
+        if (notification === "MMM-Text-To-Speech") {
             this.sendSocketNotification("MMM-Text-To-Speech", payload);
         }
     },
@@ -48,7 +46,7 @@ Module.register("MMM-Text-To-Speech", {
     // socketNotificationReceived from helper
     socketNotificationReceived: function (notification, payload) {
         if (notification === "MMM-Text-To-Speech-PLAY_SOUND") {
-            this.sendNotification('PLAY_SOUND', payload);
+            this.sendNotification("PLAY_SOUND", payload);
         }
     },
 });
--- node_helper.js
+++ node_helper.js
@@ -5,16 +5,16 @@
  * MIT Licensed.
  */
 
-require('es6-promise').polyfill();
+require("es6-promise").polyfill();
 
 var NodeHelper = require("node_helper");
-var fs = require('fs');
-var path = require('path');
-var http = require('http');
-var https = require('https');
-var urlParse = require('url').parse;
-var googleTTS = require('google-tts-api');
-var md5 = require('md5');
+var fs = require("fs");
+var path = require("path");
+var http = require("http");
+var https = require("https");
+var urlParse = require("url").parse;
+var googleTTS = require("google-tts-api");
+var md5 = require("md5");
 
 module.exports = NodeHelper.create({
     isLoaded: false,
@@ -23,96 +23,117 @@ module.exports = NodeHelper.create({
     downloadFile: function (url, dest) {
         return new Promise(function (resolve, reject) {
             var info = urlParse(url);
-            var httpClient = info.protocol === 'https:' ? https : http;
+            var httpClient = info.protocol === "https:" ? https : http;
             var options = {
                 host: info.host,
                 path: info.path,
                 headers: {
-                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
-                }
+                    "user-agent":
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
+                },
             };
-            httpClient.get(options, function (res) {
-                // check status code
-                if (res.statusCode !== 200) {
-                    reject(new Error('request to ' + url + ' failed, status code = ' + res.statusCode + ' (' + res.statusMessage + ')'));
-                    return;
-                }
+            httpClient
+                .get(options, function (res) {
+                    // check status code
+                    if (res.statusCode !== 200) {
+                        reject(
+                            new Error(
+                                "request to " +
+                                    url +
+                                    " failed, status code = " +
+                                    res.statusCode +
+                                    " (" +
+                                    res.statusMessage +
+                                    ")"
+                            )
+                        );
+                        return;
+                    }
 
-                var file = fs.createWriteStream(dest);
-                file.on('finish', function () {
-                    // close() is async, call resolve after close completes.
-                    file.close(resolve);
-                });
-                file.on('error', function (err) {
-                    // Delete the file async. (But we don't check the result)
-                    fs.unlink(dest);
-                    reject(err);
-                });
-                res.pipe(file);
-            })
-                    .on('error', function (err) {
+                    var file = fs.createWriteStream(dest);
+                    file.on("finish", function () {
+                        // close() is async, call resolve after close completes.
+                        file.close(resolve);
+                    });
+                    file.on("error", function (err) {
+                        // Delete the file async. (But we don't check the result)
+                        fs.unlink(dest);
                         reject(err);
-                    })
-                    .end();
+                    });
+                    res.pipe(file);
+                })
+                .on("error", function (err) {
+                    reject(err);
+                })
+                .end();
         });
     },
 
     tts: function (text) {
         var self = this;
         var fileName = md5(text);
-        var destDir = path.resolve(__dirname, '../' + self.config.soundModuleDir + '/sounds/tts/');
+        var destDir = path.resolve(
+            __dirname,
+            "../" + self.config.soundModuleDir + "/sounds/tts/"
+        );
         if (!fs.existsSync(destDir)) {
             fs.mkdirSync(destDir);
         }
-        var outFile = path.resolve(destDir, fileName + '.wav');
+        var outFile = path.resolve(destDir, fileName + ".wav");
         if (fs.existsSync(outFile)) {
-            self.sendSocketNotification('MMM-Text-To-Speech-PLAY_SOUND', 'tts/' + fileName + '.wav');
+            self.sendSocketNotification(
+                "MMM-Text-To-Speech-PLAY_SOUND",
+                "tts/" + fileName + ".wav"
+            );
             return;
         }
-        var destFile = path.resolve(destDir, fileName + '.mp3'); // file destination
+        var destFile = path.resolve(destDir, fileName + ".mp3"); // file destination
         googleTTS(text, self.config.language, self.config.speed)
-                .then(function (url) {
-                    console.log(url); // https://translate.google.com/translate_tts?...
-                    console.log('Download to ' + destFile + ' ...');
-                    return self.downloadFile(url, destFile);
-                })
-                .then(() => {
-                    console.log('Download success');
-                    // Mp3 to WAV
-                    const Lame = require("node-lame").Lame;
-                    const decoder = new Lame({
-                        output: outFile
-                    }).setFile(destFile);
-                    decoder.decode()
-                            .then(function () {
-                                // Play file
-                                self.sendSocketNotification('MMM-Text-To-Speech-PLAY_SOUND', 'tts/' + fileName + '.wav');
-                            })
-                            .catch(error => {
-                                // Something went wrong
-                                console.log(error);
-                            });
-                    ;
-                })
-                .catch(function (err) {
-                    console.error(err.stack);
-                });
+            .then(function (url) {
+                console.log(url); // https://translate.google.com/translate_tts?...
+                console.log("Download to " + destFile + " ...");
+                return self.downloadFile(url, destFile);
+            })
+            .then(() => {
+                console.log("Download success");
+                // Mp3 to WAV
+                const Lame = require("node-lame").Lame;
+                const decoder = new Lame({
+                    output: outFile,
+                }).setFile(destFile);
+                decoder
+                    .decode()
+                    .then(function () {
+                        // Play file
+                        self.sendSocketNotification(
+                            "MMM-Text-To-Speech-PLAY_SOUND",
+                            "tts/" + fileName + ".wav"
+                        );
+                    })
+                    .catch((error) => {
+                        // Something went wrong
+                        console.log(error);
+                    });
+            })
+            .catch(function (err) {
+                console.error(err.stack);
+            });
     },
 
     // Override socketNotificationReceived method.
     socketNotificationReceived: function (notification, payload) {
         switch (notification) {
-            case 'CONFIG':
+            case "CONFIG":
                 if (!this.isLoaded) {
                     this.config = payload;
                     this.isLoaded = true;
                 }
                 break;
-            case 'MMM-Text-To-Speech':
+            case "MMM-Text-To-Speech":
                 this.tts(payload);
                 break;
             default:
                 break;
         }
-    }
+    },
 });
